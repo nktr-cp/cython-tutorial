b277d4b2a682b955958176d8e502f9b1d78a1149
nktr-cp <nktrcp@gmail.com>
2025-06-05 11:13:31 +0900

    [add] pure python primes

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     # ext_modules = cythonize("hello_world.py")
-    ext_modules = cythonize("primes.pyx")
+    # ext_modules = cythonize("primes.pyx")
+    ext_modules = cythonize("primes.py")
 )
 
