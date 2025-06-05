a320de6d143f30ebf77fac0fb73fd27086d62192
nktr-cp <nktrcp@gmail.com>
2025-06-05 10:55:25 +0900

    fibonacci

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("hello_world.py")
+    # ext_modules = cythonize("hello_world.py")
+    ext_modules = cythonize("fibonacci.py")
 )
 
