132135c82cc65ce5ecbace8d6ae0dced94599c4c
nktr-cp <nktrcp@gmail.com>
2025-06-05 12:06:29 +0900

    [add] atoi

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("primes_cpp.pyx", annotate=True)
+    ext_modules = cythonize("atoi.pyx", annotate=False)
 )
 
